6bcbcf8fc45d2b46e555504eb1384a5e9d2f4c02
ritikaagar <Ritika@Ritikas-MacBook-Pro.local>
2017-09-18 19:43:32 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+   emails: any[] = [
+    {email: 'Bill@Gates.com', importance: true, subject: 'I am giving you money', content: 'just kidding'},
+    {email: 'Jessica@Alba.com', importance: true, subject: 'I want to date you', content: 'i am super serious'},
+    {email: 'Kobe@Bryant.com', importance: false, subject: 'Work harder', content: 'you suck at basketball'},
+    {email: 'Michael@Choi.com', importance: false, subject: 'good job', content: 'you are doing great!'}
+  ]
 }
